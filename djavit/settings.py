20877f87315c82672562e6ba37a2cd7a81e5cf89
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-06-12 10:02:37 -0300

    Created and configured from AWS / S3 Close #9

--- djavit/settings.py
+++ djavit/settings.py
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Development environment configuration
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# MEDIA_URL - '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
