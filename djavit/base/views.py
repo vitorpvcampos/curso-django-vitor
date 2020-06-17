6000cc8c32e48c2a365d4f63b04292c1e7ab3f3d
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-06-17 17:51:30 -0300

    Landing page created
    close #39

--- djavit/base/views.py
+++ djavit/base/views.py
@@ -1,6 +1,5 @@
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><body>Hello Django</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
