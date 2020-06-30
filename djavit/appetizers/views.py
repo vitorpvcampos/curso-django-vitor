fd9560eeff9cf858278958cc271e30aa4c10fe95
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-06-30 15:34:25 -0300

    Created page for second appetizer video close #74

--- djavit/appetizers/views.py
+++ djavit/appetizers/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(resquest, slug):
-    video = {'title': 'Appetizer Video: Test', 'vimeo_id': 431952852}
+    videos = {
+        'test': {'title': 'Appetizer Video: Test', 'vimeo_id': 431952852},
+        'beach': {'title': 'Appetizer Video: Beach', 'vimeo_id': 434093274},
+    }
+    video = videos[slug]
     return render(resquest, 'appetizers/video.html', context={'video': video})
