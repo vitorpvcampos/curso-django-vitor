9ffc07efc0709d2ac8e878c1c66526efc6812e2f
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-06-30 15:21:28 -0300

    Template Context

--- djavit/appetizers/views.py
+++ djavit/appetizers/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(resquest, slug):
-    return render(resquest, 'appetizers/video.html')
+    video = {'title': 'Appetizer Video: Test', 'vimeo_id': 431952852}
+    return render(resquest, 'appetizers/video.html', context={'video': video})
