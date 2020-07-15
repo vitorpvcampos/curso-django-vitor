7312f7f08913b5522bb0078dcbb05878c14f8ec5
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-07-15 12:20:43 -0300

    Created Relationship between Module and Classes close #105

--- djavit/modules/views.py
+++ djavit/modules/views.py
@@ -5,4 +5,5 @@ from djavit.modules import facade
 
 def detail(request, slug):
     module = facade.find_module(slug)
-    return render(request, 'modules/module_detail.html', {'module': module})
+    classes = facade.list_ordered_module_classes(module)
+    return render(request, 'modules/module_detail.html', {'module': module, 'classes': classes})
