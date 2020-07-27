a2bc3f9761c37eb8253ba34cb213c3c17f4b4ce2
vitorpvcampos <vitor.campos@engenharia.ufjf.br>
2020-07-27 14:14:18 -0300

    Created Login Form close #140

--- djavit/urls.py
+++ djavit/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('djavit.base.urls')),
     path('appetizers/', include('djavit.appetizers.urls')),
     path('modules/', include('djavit.modules.urls')),
